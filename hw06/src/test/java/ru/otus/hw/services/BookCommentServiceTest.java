65324019bf550403e7ad53e44d3ad6f3929eb31a
dzolotarev <dzolotarev@gmail.com>
2025-01-11 20:10:40 +0300

    Exercise 6 Change 2

--- hw06/src/test/java/ru/otus/hw/services/BookCommentServiceTest.java
+++ hw06/src/test/java/ru/otus/hw/services/BookCommentServiceTest.java
@@ -2,33 +2,41 @@ package ru.otus.hw.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.converters.AuthorConverter;
+import ru.otus.hw.converters.BookCommentConverter;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.converters.GenreConverter;
 import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.dto.BookCommentDto;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.GenreDto;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.BookComment;
+import ru.otus.hw.repositories.JpaBookCommentRepository;
+import ru.otus.hw.repositories.JpaBookRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-// Пока не хватило запала реализовать все операции, но остальные функции из сервисов
-// просто вызывают соответствующие методы репозитория, которые уже покрыты тестами и конвертят в DTO.
-// Непонятно насколько осмысленно повторять это в тестах сервиса, логичнее отдельно конвертацию в DTO тогда тестить
 @DisplayName("Сервис для работы с комментариями к книгам ")
 @DataJpaTest
-@ComponentScan(basePackages = "ru.otus.hw")
+@Import({BookCommentConverter.class, BookConverter.class, AuthorConverter.class, GenreConverter.class,
+        BookCommentServiceImpl.class, JpaBookCommentRepository.class, JpaBookRepository.class})
 public class BookCommentServiceTest {
 
     private static final long COMMENTED_BOOK_ID = 2L;
     private static final long COMMENT_ID = 3L;
+    private static final long LAST_COMMENT_ID_FOR_COMMENTED_BOOK = 4;
 
     @Autowired
     private TestEntityManager em;
@@ -46,19 +54,7 @@ public class BookCommentServiceTest {
         var dbBookComment = em.find(BookComment.class, savedBookCommentDto.getId());
         assertThat(dbBookComment).isNotNull();
 
-        // Руками в DTO, чтоб не использовать функцию, которую не тестируем
-        var authorDto = new AuthorDto(
-                dbBookComment.getBook().getAuthor().getId(),
-                dbBookComment.getBook().getAuthor().getFullName());
-
-        List<GenreDto> genresDto = dbBookComment.getBook().getGenres().stream()
-                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
-
-        var bookDto = new BookDto(dbBookComment.getBook().getId()
-                , dbBookComment.getBook().getTitle(), authorDto, genresDto);
-
-        var bookCommentDto = new BookCommentDto(dbBookComment.getId(), bookDto, dbBookComment.getComment());
-
+        var bookCommentDto = bookCommentToDto(dbBookComment);
         assertThat(bookCommentDto).usingRecursiveComparison()
                 .ignoringExpectedNullFields().isEqualTo(savedBookCommentDto);
 
@@ -79,19 +75,7 @@ public class BookCommentServiceTest {
         var dbBookComment = em.find(BookComment.class, COMMENT_ID);
         assertThat(dbBookComment).isNotNull();
 
-        // Руками в DTO, чтоб не использовать функцию, которую не тестируем
-        var authorDto = new AuthorDto(
-                dbBookComment.getBook().getAuthor().getId(),
-                dbBookComment.getBook().getAuthor().getFullName());
-
-        List<GenreDto> genresDto = dbBookComment.getBook().getGenres().stream()
-                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
-
-        var bookDto = new BookDto(dbBookComment.getBook().getId()
-                , dbBookComment.getBook().getTitle(), authorDto, genresDto);
-
-        var bookCommentDto = new BookCommentDto(dbBookComment.getId(), bookDto, dbBookComment.getComment());
-
+        var bookCommentDto = bookCommentToDto(dbBookComment);
         assertThat(bookCommentDto).usingRecursiveComparison()
                 .ignoringExpectedNullFields().isEqualTo(savedBookComment);
 
@@ -99,6 +83,57 @@ public class BookCommentServiceTest {
             bookCommentService.update(2000000000000L, modifiedComment);
         });
         assertEquals("Book comment with id 2000000000000 not found", thrownBook.getMessage());
+    }
+
+    @DisplayName("должен загружать комментарий по id")
+    @ParameterizedTest
+    @ValueSource(longs = {1, 3, 5, 100000000})
+    void shouldReturnCorrectCommentById(long id) {
+        var actualBookComment = bookCommentService.findById(id);
+        var expectedBookComment = Optional.ofNullable(em.find(BookComment.class, id));
+        assertTrue(actualBookComment.isPresent() && expectedBookComment.isPresent()
+                || actualBookComment.isEmpty() && expectedBookComment.isEmpty());
+
+        if (actualBookComment.isPresent()) {
+            var expectedBookCommentDto = bookCommentToDto(expectedBookComment.get());
+            assertThat(actualBookComment).get().usingRecursiveComparison()
+                    .ignoringExpectedNullFields().isEqualTo(expectedBookCommentDto);
+        }
+    }
+
+    @DisplayName("должен загружать список всех комментариев книги")
+    @Test
+    void shouldReturnCorrectBookCommentsList() {
+        var actualBookComments = bookCommentService.findForBook(COMMENTED_BOOK_ID);
+        var expectedBookComments = LongStream.range(3, LAST_COMMENT_ID_FOR_COMMENTED_BOOK + 1).boxed()
+                .map(id -> bookCommentToDto(em.find(BookComment.class, id))).toList();
+        assertThat(actualBookComments).usingRecursiveComparison()
+                .ignoringExpectedNullFields().isEqualTo(expectedBookComments);
+    }
+
+    @DisplayName("должен удалять комментарий по id ")
+    @Test
+    void shouldDeleteBookComment() {
+        var currenBookComment = em.find(BookComment.class, COMMENT_ID);;
+        assertThat(currenBookComment).isNotNull();
+        em.detach(currenBookComment);
+
+        bookCommentService.deleteById(currenBookComment.getId());
+        var deletedBookComment = em.find(BookComment.class, currenBookComment.getId());
+        assertThat(deletedBookComment).isNull();
+    }
+
+    // Чтоб не использовать функцию, которую не тестируем
+    private BookCommentDto bookCommentToDto(BookComment bookComment) {
+        var authorDto = new AuthorDto(
+                bookComment.getBook().getAuthor().getId(),
+                bookComment.getBook().getAuthor().getFullName());
+        List<GenreDto> genresDto = bookComment.getBook().getGenres().stream()
+                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
+        var bookDto = new BookDto(bookComment.getBook().getId()
+                , bookComment.getBook().getTitle(), authorDto, genresDto);
+
+        return new BookCommentDto(bookComment.getId(), bookDto, bookComment.getComment());
     }
 
 }
--- hw06/src/test/java/ru/otus/hw/services/BookServiceTest.java
+++ hw06/src/test/java/ru/otus/hw/services/BookServiceTest.java
@@ -2,35 +2,42 @@ package ru.otus.hw.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.converters.AuthorConverter;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.converters.GenreConverter;
 import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.GenreDto;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Book;
+import ru.otus.hw.repositories.JpaAuthorRepository;
+import ru.otus.hw.repositories.JpaBookRepository;
+import ru.otus.hw.repositories.JpaGenreRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-// Пока не хватило запала реализовать все операции, но остальные функции из сервисов
-// просто вызывают соответствующие методы репозитория, которые уже покрыты тестами и конвертят в DTO.
-// Непонятно насколько осмысленно повторять это в тестах сервиса, логичнее отдельно конвертацию в DTO тогда тестить
 @DisplayName("Сервис для работы с книгами ")
 @DataJpaTest
-@ComponentScan(basePackages = "ru.otus.hw")
+@Import({AuthorConverter.class, GenreConverter.class, BookConverter.class, BookServiceImpl.class,
+        JpaAuthorRepository.class, JpaBookRepository.class, JpaGenreRepository.class})
 public class BookServiceTest {
 
     private static final long BOOK_ID = 2L;
     private static final long NEW_AUTHOR_ID = 2L;
     private static final Set<Long> NEW_GENRE_IDS = Set.of(1L, 3L, 4L);
+    private static final long EXPECTED_NUMBER_OF_BOOKS = 3;
 
     @Autowired
     private TestEntityManager em;
@@ -46,12 +53,7 @@ public class BookServiceTest {
         var savedBook = bookService.insert(newBookTitle, NEW_AUTHOR_ID, NEW_GENRE_IDS);
         var dbBook = em.find(Book.class, savedBook.getId());
         assertThat(dbBook).isNotNull();
-
-        // Руками в DTO, чтоб не использовать функцию, которую не тестируем
-        var authorDto = new AuthorDto(dbBook.getAuthor().getId(), dbBook.getAuthor().getFullName());
-        List<GenreDto> genresDto = dbBook.getGenres().stream()
-                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
-        var bookDto = new BookDto(dbBook.getId(),dbBook. getTitle(), authorDto, genresDto);
+        var bookDto = bookToBookDto(dbBook);
 
         assertThat(bookDto).usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(savedBook);
 
@@ -81,12 +83,7 @@ public class BookServiceTest {
         var savedBook = bookService.update(BOOK_ID, modifiedBookTitle, NEW_AUTHOR_ID, NEW_GENRE_IDS);
         var dbBook = em.find(Book.class, BOOK_ID);
         assertThat(dbBook).isNotNull();
-
-        // Руками в DTO, чтоб не использовать функцию, которую не тестируем
-        var authorDto = new AuthorDto(dbBook.getAuthor().getId(), dbBook.getAuthor().getFullName());
-        List<GenreDto> genresDto = dbBook.getGenres().stream()
-                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
-        var bookDto = new BookDto(dbBook.getId(),dbBook. getTitle(), authorDto, genresDto);
+        var bookDto = bookToBookDto(dbBook);
 
         assertThat(bookDto).usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(savedBook);
 
@@ -105,4 +102,49 @@ public class BookServiceTest {
         });
         assertEquals("One or all genres with ids [100000000] not found", thrownGenre2.getMessage());
     }
+
+    @DisplayName("должен загружать книгу по id")
+    @ParameterizedTest
+    @ValueSource(longs = {1, 3, 5, 100000000})
+    void shouldReturnCorrectBookById(long id) {
+        var actualBook = bookService.findById(id);
+        var expectedBook = Optional.ofNullable(em.find(Book.class, id));
+        assertTrue(actualBook.isPresent() && expectedBook.isPresent()
+                || actualBook.isEmpty() && expectedBook.isEmpty());
+        if (actualBook.isPresent()) {
+            var expectedBookDto = bookToBookDto(expectedBook.get());
+            assertThat(actualBook).get().usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedBookDto);
+        }
+    }
+
+    @DisplayName("должен загружать список всех книг")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        var actualBooks = bookService.findAll();
+        var expectedBooksDto = LongStream.range(1, EXPECTED_NUMBER_OF_BOOKS + 1).boxed()
+                .map(id -> bookToBookDto(em.find(Book.class, id))).toList();
+
+        assertThat(actualBooks).usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedBooksDto);
+    }
+
+    @DisplayName("должен удалять книгу по id ")
+    @Test
+    void shouldDeleteBook() {
+        var currenBook = em.find(Book.class, BOOK_ID);
+        assertThat(currenBook).isNotNull();
+        em.detach(currenBook);
+
+        bookService.deleteById(BOOK_ID);
+        var deletedBook = em.find(Book.class, BOOK_ID);
+        assertThat(deletedBook).isNull();
+    }
+
+    // Чтоб не использовать функцию, которую не тестируем
+    private BookDto bookToBookDto (Book book) {
+        var authorDto = new AuthorDto(book.getAuthor().getId(), book.getAuthor().getFullName());
+        List<GenreDto> genresDto = book.getGenres().stream()
+                .map(genre -> new GenreDto(genre.getId(), genre.getName())).toList();
+        return new BookDto(book.getId(),book. getTitle(), authorDto, genresDto);
+    }
+
 }
